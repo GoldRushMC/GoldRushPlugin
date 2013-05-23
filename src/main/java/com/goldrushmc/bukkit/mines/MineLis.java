021c56f2e9e4aec832f5078e07d192ec29e78077
Lex Webb <lexwebb@gmail.com>
2013-05-23 17:13:02 +0100

    Added caves and multi ore vein spawn to mines

--- src/main/java/com/goldrushmc/bukkit/mines/MineLis.java
+++ src/main/java/com/goldrushmc/bukkit/mines/MineLis.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -12,12 +14,19 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 import com.goldrushmc.bukkit.defaults.DefaultListener;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import com.sk89q.worldedit.bukkit.selections.CuboidSelection;
+import com.sk89q.worldedit.bukkit.selections.Selection;
 
 public class MineLis extends DefaultListener{
 
 	public static Map<Player, Location> mineLoc = new HashMap<Player, Location>();
+	public static Map<Player, Vector> mineMin = new HashMap<Player, Vector>();
+	public static Map<Player, Vector> mineMax = new HashMap<Player, Vector>();
+	public static WorldEditPlugin worldEditPlugin = null;
 	
 	public MineLis(JavaPlugin plugin) {
 		super(plugin);
@@ -25,19 +34,49 @@ public class MineLis extends DefaultListener{
 	}
 	
 	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onRightClick(PlayerInteractEvent e) {
+	public void onRightClick(PlayerInteractEvent e) {		
 		Action eAction = e.getAction();
 		Player p = e.getPlayer();
+		
+        
+        
 		if(eAction.equals(Action.RIGHT_CLICK_BLOCK)) {
 			if (p.getItemInHand().getType().equals(Material.CLAY_BALL)) {
 				if (p.getItemInHand().getItemMeta().hasDisplayName()) {
 					if (p.getItemInHand().getItemMeta().getDisplayName().equals("Mine Creator")) {
 						mineLoc.put(p, e.getClickedBlock().getLocation());
-						p.sendMessage("Mine start donated at: " +
-							mineLoc.get(p).getX() + ", " +
-							mineLoc.get(p).getZ() + ", " +
-							mineLoc.get(p).getY() + ".");
-						p.sendMessage("Use '/mine create {Size - X} {Size - Z} {Size - Y}' to create a mine");
+						
+						//initialise WorldEdit
+						worldEditPlugin = (WorldEditPlugin) Bukkit.getServer().getPluginManager().getPlugin("WorldEdit");
+				        if(worldEditPlugin == null){
+				            p.sendMessage("Error: WorldEdit is null.");   
+				        }
+				        Selection sel = worldEditPlugin.getSelection(p);
+
+				        //allow proceed if selection is of correct type
+				        if (sel instanceof CuboidSelection) {
+				            mineMin.put(p, new Vector(sel.getNativeMinimumPoint().getBlockX(),
+				            		sel.getNativeMinimumPoint().getBlockY(),
+				            		sel.getNativeMinimumPoint().getBlockZ()));
+				            mineMax.put(p, new Vector(sel.getNativeMaximumPoint().getBlockX(),
+				            		sel.getNativeMaximumPoint().getBlockY(),
+				            		sel.getNativeMaximumPoint().getBlockZ()));         
+
+				            //make sure the Entrance block is inside the WorlEdit selection
+				            if(sel.contains(e.getClickedBlock().getLocation())) {
+								p.sendMessage("Mine entrance donated at: " +
+										mineLoc.get(p).getX() + ", " +
+										mineLoc.get(p).getY() + ", " +
+										mineLoc.get(p).getZ() + ".");
+								p.sendMessage("Use '/mine create to create mine");
+							} else {
+								p.sendMessage(ChatColor.RED + "Entrance is not within WorldEdit Selection"); 
+							}
+				            
+				        }else{
+				            p.sendMessage(ChatColor.DARK_RED + "Invalid Selection!");
+				        }
+						
 					}
 				}
 			}
--- src/main/java/com/goldrushmc/bukkit/mines/MineTool.java
+++ src/main/java/com/goldrushmc/bukkit/mines/MineTool.java
@@ -4,17 +4,22 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.BlockIterator;
+import org.bukkit.util.Vector;
 
 import com.goldrushmc.bukkit.defaults.CommandDefault;
 
@@ -37,25 +42,19 @@ public class MineTool  extends CommandDefault {
 			if (p instanceof Player){ // instance of player - checks if player
 				if (args.length > 0){
 					if (args[0].equalsIgnoreCase("create")){ 
-						if (args.length == 4){
 							try{
-								List<Integer> list = new ArrayList<Integer>();
-								list.add(Integer.valueOf(args[1])); 
-								list.add(Integer.valueOf(args[2]));
-								list.add(Integer.valueOf(args[3]));
-								mineSize.put(p, list);
-								p.sendMessage("Created mine outline of size: "+ args[1] + ", " + args[2] + ", " + args[3] + ".");
-								p.sendMessage("Type /mine confirm to create the mine, /mine cancel to start again.");
-								makeMarkers(p.getWorld(), p, Material.WOOL);
+								p.sendMessage("Created mine outline");
+								p.sendMessage("Type " + ChatColor.GREEN + "/mine confirm " + 
+										ChatColor.WHITE + "to create the mine, " +
+										ChatColor.RED + "/mine cancel " + ChatColor.WHITE + "to start again.");
+								makeMarkers(p.getWorld(), p, Material.WOOL); //makes a wool outline of the area selected
 								return true;
 							} catch(Exception ex) {
 								p.sendMessage(ChatColor.RED + "Invalid Paramters!");
 								return false;
 							}
-						} else {
-							return false;
-						}
 					} else if (args[0].equalsIgnoreCase("tool")){ 
+						
 						ItemStack panningTool= new ItemStack(Material.CLAY_BALL);
 						List<String> lore = new ArrayList<String>();
 						lore.add("Right click to denote start of mine");
@@ -65,11 +64,24 @@ public class MineTool  extends CommandDefault {
 						panningTool.setItemMeta(meta);
 						p.getInventory().addItem(panningTool);
 						return true;
-					} else if (args[0].equalsIgnoreCase("comfirm")){ 
+						
+					} else if (args[0].equalsIgnoreCase("confirm")){ 
+						
+						int density = 5; //effects the spawn rate of ores and caves, the higher, the less
+						
+						int size = (MineLis.worldEditPlugin.getSelection(p).getWidth() + 
+								MineLis.worldEditPlugin.getSelection(p).getHeight() + 
+								MineLis.worldEditPlugin.getSelection(p).getLength()) / 3; //Mean of height/Length/Width
+						
+						int veins = size / density;
+						
+						fillSelection(p.getWorld(), p, Material.GLASS); //fills the area with chosen base block
+						populateGold(p.getWorld(), p, veins, 10); //populates area with gold veins
+						populateTunnels(p.getWorld(), p, 6, size / (density * 2) ); //populates the area with tunnels
 						
 						return true;
 					} else if (args[0].equalsIgnoreCase("cancel")){ 
-						undoMarkers(p.getWorld(), p);
+						undoMarkers(p.getWorld(), p); //undoes the placement of wool outline
 						return true;
 					} else {
 						p.sendMessage("Not a valid command!");
@@ -86,14 +98,134 @@ public class MineTool  extends CommandDefault {
 		}
 	}
 	
-	private void makeMarkers(World w, Player p, Material m) {
-		int maxx = MineLis.mineLoc.get(p).getBlockX() + (mineSize.get(p).get(0) / 2);
-		int minx = MineLis.mineLoc.get(p).getBlockX() - (mineSize.get(p).get(0) / 2);
-		int maxz = MineLis.mineLoc.get(p).getBlockZ() + (mineSize.get(p).get(1) / 2);
-		int minz = MineLis.mineLoc.get(p).getBlockZ() - (mineSize.get(p).get(1) / 2);
-		int maxy = MineLis.mineLoc.get(p).getBlockY() + (mineSize.get(p).get(2) / 2);
-		int miny = MineLis.mineLoc.get(p).getBlockY() - (mineSize.get(p).get(2) / 2);
+	private void fillSelection(World w, Player p, Material m) {
+		int maxx = MineLis.mineMax.get(p).getBlockX();
+		int minx = MineLis.mineMin.get(p).getBlockX();
+		int maxz = MineLis.mineMax.get(p).getBlockZ();
+		int minz = MineLis.mineMin.get(p).getBlockZ();
+		int maxy = MineLis.mineMax.get(p).getBlockY();
+		int miny = MineLis.mineMin.get(p).getBlockY();
 		
+		//simple loop to fill area
+		for(int x = minx; x <= maxx; x++) {
+			for(int z = minz; z <= maxz; z++) {
+				for(int y = miny; y <= maxy; y++) {
+						w.getBlockAt(x, y, z).setType(m);
+				}
+			}
+		}
+	}
+	
+	private void populateGold(World w, Player p, Integer numVeins, Integer veinLength) {
+		
+		//loops on number of veins specified
+		for(int i = 0; i < numVeins; i++){
+			//uses a random set of vectors to get the points for the gold vein to be drew.
+			Vector location = randomConstrainedVector(MineLis.mineMin.get(p), MineLis.mineMax.get(p));
+			Vector newLocation = randomConstrainedVector(MineLis.mineMin.get(p), MineLis.mineMax.get(p));
+			
+			//limit the length of ore veins
+			int distance = (int) Math.floor(location.distance(newLocation) - 1);
+			if((int) Math.floor(location.distance(newLocation) - 1) > veinLength) {
+				distance = veinLength;
+			}
+			
+			BlockIterator bli = new BlockIterator(w, location, new Vector(newLocation.getBlockX()-location.getBlockX(), newLocation.getBlockY()-location.getBlockY(), newLocation.getBlockZ()-location.getBlockZ()), 0, distance - 1);
+		
+			//BlockIterator draws lines of a block between two specified Vectors
+			Location blockToAdd;
+			while(bli.hasNext()) {
+				blockToAdd = bli.next().getLocation();
+				w.getBlockAt(new Location(blockToAdd.getWorld(), blockToAdd.getBlockX(), blockToAdd.getBlockY(), blockToAdd.getBlockZ())).setType(Material.GOLD_ORE);
+			}
+		}
+	}
+	
+	private void populateTunnels(World w, Player p, Integer numBranches, Integer numTunnels) {
+		//initial tunnel start point
+		Vector startLocation = MineLis.mineLoc.get(p).toVector();
+		Vector minArea = new Vector(0,0,0), maxArea = new Vector(0,0,0);
+		
+		//loops through num of tunnels
+		for(int tunnels = 0; tunnels < numTunnels; tunnels++) {
+			//loops through num of branches, each one starts at the previous end.
+			for(int branch = 0; branch < numBranches; branch++) {
+				//specifies the maximum distances the random Vector can be from the Last point
+				minArea = new Vector(startLocation.getBlockX() - 8, startLocation.getBlockY() - 3, startLocation.getBlockZ() - 8);
+				maxArea = new Vector(startLocation.getBlockX() + 8, startLocation.getBlockY() + 3, startLocation.getBlockZ() + 8);		
+			
+				Vector endLocation = new Vector(0,0,0);
+				
+				int limit = 0; // counter used in loop to prevent infinite state
+				Boolean nextPointFound = false;
+				
+				//Loops until the random point is within the mine area
+				while(true) {
+					endLocation = randomConstrainedVector(minArea, maxArea);
+					if(MineLis.worldEditPlugin.getSelection(p).contains(endLocation.toLocation(w))) {
+						nextPointFound = true;
+						break;
+					}				
+					limit++;
+					if(limit == 50) { nextPointFound = false; break; }
+				}
+		
+				if(nextPointFound){
+					BlockIterator bli = new BlockIterator(w, startLocation, new Vector(endLocation.getBlockX()-startLocation.getBlockX(), endLocation.getBlockY()-startLocation.getBlockY(), endLocation.getBlockZ()-startLocation.getBlockZ()), 0, (int) Math.floor(startLocation.distance(endLocation)+ 1));
+		
+					Location blockToAdd;
+					while(bli.hasNext()) {
+						blockToAdd = bli.next().getLocation();
+						Block blockToChange = w.getBlockAt(new Location(blockToAdd.getWorld(), blockToAdd.getBlockX(), blockToAdd.getBlockY(), blockToAdd.getBlockZ()));
+						blockToChange.setType(Material.AIR);
+						
+						//also changes adjacent blocks
+						for(BlockFace face : BlockFace.values())
+							blockToChange.getRelative(face).setType(Material.AIR);
+					}
+				
+					//makes next segment start the end of previous
+					startLocation = endLocation;
+				} else {
+					break;
+				}
+			}
+			//randomises location of next tunnel
+			startLocation = randomConstrainedVector(MineLis.mineMin.get(p), MineLis.mineMax.get(p));
+		}		
+	}
+	
+	//returns a vector that is within the quad of the two specified vectors
+	private Vector randomConstrainedVector(Vector minVector, Vector maxVector) {
+		Random rand = new Random();
+		int x, y, z;
+		int min, max;
+
+		max = maxVector.getBlockX();
+		min = minVector.getBlockX();
+		x = rand.nextInt(max - min + 1) + min;
+		
+		max = maxVector.getBlockY();
+		min = minVector.getBlockY();
+		y = rand.nextInt(max - min + 1) + min;
+		
+		max = maxVector.getBlockZ();
+		min = minVector.getBlockZ();
+		z = rand.nextInt(max - min + 1) + min;
+		
+		return new Vector(x, y, z);
+	}
+	
+	private void makeMarkers(World w, Player p, Material m) {
+		
+		int maxx = MineLis.mineMax.get(p).getBlockX();
+		int minx = MineLis.mineMin.get(p).getBlockX();
+		int maxz = MineLis.mineMax.get(p).getBlockZ();
+		int minz = MineLis.mineMin.get(p).getBlockZ();
+		int maxy = MineLis.mineMax.get(p).getBlockY();
+		int miny = MineLis.mineMin.get(p).getBlockY();
+		
+		//Backup array
 		List<blockBackup> temp = new ArrayList<blockBackup>();
 		
 		if(backList.containsKey(p)){
@@ -102,6 +234,7 @@ public class MineTool  extends CommandDefault {
 			}
 		}
 		
+		//loops to only fill the edges of the cube
 		backList.remove(p);
 		for(int x = minx; x <= maxx; x++) {
 			for(int z = minz; z <= maxz; z++) {
@@ -156,6 +289,7 @@ public class MineTool  extends CommandDefault {
 		}
 	}
 	
+	//class for storing the block backup data
 	class blockBackup {
 		public Location location;
 		public Material material;
