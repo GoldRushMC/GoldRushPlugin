9e08a4674e0eb38e21de3daa375cf0fda768c6f7
Lucas <Lucas.moran@snhu.edu>
2013-05-28 17:40:39 -0300

    Moved LoadMineTask to the mines package, out of Main.java.

--- src/main/java/com/goldrushmc/bukkit/main/Main.java
+++ src/main/java/com/goldrushmc/bukkit/main/Main.java
@@ -180,30 +180,4 @@ public final class Main extends JavaPlugin{
 		}
 		this.getLogger().info("GoldRush Plugin Disabled!");
 	}
-
-	class LoadMinesTask implements Runnable{
-		JavaPlugin p;
-		LoadMinesTask(JavaPlugin plug) {
-			p = plug;
-		}
-
-		@Override
-		public void run() {
-			LoadMines loadMines = new LoadMines(p, p);
-			Bukkit.getServer().getScheduler().runTaskLater(p, new GenTask(loadMines), 20);
-		}
-		
-		class GenTask implements Runnable{
-			LoadMines loadMines;
-			GenTask(LoadMines lm) {
-				loadMines = lm;
-			}
-
-			@Override
-			public void run() {
-				mineList = loadMines.parseMinesStrings();
-				Bukkit.getServer().broadcastMessage("Successfully loaded " + mineList.size() + " mines!");
-			}
-		}
-	}
 }
